95c296aa5ac4551daf3e5c5826f5fcd185d2be1d
bernard <b.karam@iconsultants.eu>
2013-09-30 09:16:01 +0200

    allRowsMinInfo.php repair1

--- modules/ranking/libs/allRowsMinInfo.php
+++ modules/ranking/libs/allRowsMinInfo.php
@@ -1,6 +1,21 @@
 <?php
 
+ini_set("display_errors",1);
 
+try
+{
+    require_once("../../../init.php");
+}
+catch (Exception $e)
+{
+    echo '<pre>';
+    print_r($e->getMessage());
+    echo '</pre>';
+    echo '<pre>';
+    print_r($e->getTrace());
+    echo '</pre>';
+    exit();
+}
 
 
 //set today
