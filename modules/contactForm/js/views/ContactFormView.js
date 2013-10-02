e3962b299ce7fae0095360844b20ecf1900e607c
gpollock <guntrampollock@gmail.com>
2013-10-02 17:31:08 +0200

    test

--- modules/contactForm/js/views/ContactFormView.js
+++ modules/contactForm/js/views/ContactFormView.js
@@ -2,7 +2,7 @@ define([
     'jquery',
     'underscore',
     'backbone',
-    'text!modulesSrc/contactForm/templates/contactForm_bak.html'
+    'text!modulesSrc/contactForm/templates/contactForm_test.html'
 ], function ($, _, Backbone, FormTemplate) {
     var FormView = Backbone.View.extend({
 
