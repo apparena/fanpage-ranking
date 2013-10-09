cbc300caa83182ae99422730fc7663dcf4827051
maerz-illustratics <maerz@illustratics.com>
2013-10-09 21:13:36 +0200

    no message

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ if (isset($aaForJs['instance']['fb_app_secret']))
 <html lang="de-DE">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=1170, maximum-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Volksbanks fan page ranking App</title>
 
     <link type="text/css" rel="stylesheet" href="css/style.css" />
@@ -76,101 +76,143 @@ if (isset($aaForJs['instance']['fb_app_secret']))
     <?php if(__c('css_bootstrap', 'src') !== false): ?><link type="text/css" rel="stylesheet" href="<?php __pc('css_bootstrap', 'src')?>" /><?php ENDIF; ?>
     <?php if (__c('css_app', 'src') !== false): ?><link type="text/css" rel="stylesheet" href="<?php __pc('css_app', 'src')?>" /><?php ENDIF; ?>
 
-    <link type="text/css" rel="stylesheet" href="css/main-layout.css" />
-
 </head>
 
-
 <body class="<?=$classbody?>">
+<div class="navbar navbar-inverse navbar-fixed-top" style="display:none">
+    <div class="container">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Logo</a>
 
-  <div class="customer_entry_container">
-     <?php __p( 'customer_entry_msg' ); ?>
-     <a href="#page/contactForm" id="customer_form_btn" class="btn btn-primary"><?php __p( 'customer_entry_btn' ); ?></a>
-  </div>
+        <div class="nav-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#/page/demo">load Demomodule</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div id="msg-container"></div>
+<div id="fb-root"></div>
 
-  <div id="custom_header">
-	   <div class="main_container">
-	   	<img src="img/sparkasse_logo.png" alt="Sparkasse" class="left" />
-	   	<div>
-		   	<h1><?php __p( 'title' ); ?></h1>
-			   <h2>SEO</h2>
-			   <p>
-			   	<?php echo (isset( $aa[ 'config' ][ 'custom_header' ][ 'value' ] ) ) ? $aa[ 'config' ][ 'custom_header' ][ 'value' ] : ''; ?>
-			   </p>
-	   	</div>
-	   </div>
-	</div>
 
- <div id="time_bar">
- 	<div class="main_container">
-      <ul class="nav nav-pills">
-         <li class="typeOfTime active" id="30days"><a href="#"><?php __p( 'chooser_30_days' ); ?></a></li>
-         <li class="typeOfTime" id="10weeks"><a href="#"><?php __p( 'chooser_10_weeks' ); ?></a></li>
-         <li class="typeOfTime" id="6months"><a href="#"><?php __p( 'chooser_6_months' ); ?></a></li>
-         <li class="typeOfTime" id="12months"><a href="#"><?php __p( 'chooser_12_months' ); ?></a></li>
-     </ul>
-     <?php __p( 'overall_time_chooser' ); ?>
- 	</div>
- </div>
 
- 	<div id="choose_bar">
- 		<div class="main_container">
- 			<div class="right">
- 				<?php __p( 'banks_to_rank' ); ?> &nbsp;
-              <select multiple="" name="e9" id="e9" class="populate select2-offscreen" tabindex="-1">
-              </select>
- 			</div>
-          <?php
-               $dropdown = '&nbsp;</p>
-                       <select id=\"max-rows\">
-                           <option value=\"all\">' . __t( 'all' ) . '</option>
-                           <option value=\"1\">1</option>
-                           <option value=\"2\">2</option>
-                           <option value=\"3\">3</option>
-                           <option value=\"4\">4</option>
-                           <option value=\"5\">5</option>
-                           <option value=\"10\">10</option>
-                           <option value=\"15\">15</option>
-                           <option value=\"20\">20</option>
-                           <option value=\"25\">25</option>
-                       </select>
-                   <p>&nbsp;';
-               __p( 'chooser_limit', $dropdown );
-           ?> &nbsp;  &nbsp; &nbsp;
- 			<button type="button" id="collapse-all" class="btn btn-info btn-xs"><?php __p( 'collapse_all' ); ?></button>
-     </div>
- </div>
 
 <div class="container">
 
+<br>
+<div class='list-group-item' style= "font-weight:bold">
+    <div class="alert">
+        <h3>Ranking Sparkasse Banks</h3>
+        <h12 class="num-elements"></h12> Banks ranked on : <h12 class="date"></h12> at: <h12 class="time"></h12>
+    </div>
+
+    <div class="row">
+
+        <div class="col-6">
+            <div class="row">
+                <div class="col-12">
+                    <p>Choose time scale of Graphs</p>
+                    <ul class="nav nav-pills">
+                        <li class="typeOfTime active" id="30days"><a href="#">Last 30 days</a></li>
+                        <li class="typeOfTime" id="10weeks"><a href="#">Last 10 weeks</a></li>
+                        <li class="typeOfTime" id="6months"><a href="#">Last 6 months</a></li>
+                        <li class="typeOfTime" id="12months"><a href="#">Last 12 months</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="collapsed" id="spinner-typeOfTime" style="position:relative;left:50%;top:50%;"><i class='icon-spinner icon-spin icon-large' style='font-size:44px;'></i></div>
+        </div>
+
+        <div class="col-6">
+
+            <div class="row">
+                <div class="col-8 top-bottom" style="float:left">
+                    <p style="float:left">Display the Top / Bottom &nbsp;</p>
+                    <select id="max-rows" style="float:left">
+                        <option value="all">all</option>
+                        <option value=1>1</option>
+                        <option value=2>2</option>
+                        <option value=3>3</option>
+                        <option value=4>4</option>
+                        <option value=5>5</option>
+                        <option value=10>10</option>
+                        <option value=15>15</option>
+                        <option value=20>20</option>
+                        <option value=25>25</option>
+                    </select>
+                    <p>&nbsp;Banks</p>
+                </div>
+
+                <div class="col-4">
+                    <button type="button" id="collapse-all" class="btn btn-info btn-xs">Collapse All</button>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-12">
+                    <p>Choose individual banks you want to rank</p>
+                        <div class="select2-container select2-container-multi populate" id="s2id_e9" style="width:300px; display:none">
+                            <ul class="select2-choices">
+
+                                <li class="select2-search-field">
+                                <input type="text" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" class="select2-input" id="s2id_autogen1" tabindex="-1" style="width: 10px;">
+                                </li>
+                            </ul>
+                        </div>
+                         <select multiple="" name="e9" id="e9" style="width:500px" class="populate select2-offscreen" tabindex="-1">
+                         </select>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+</div>
+
+
+
+<br>
+
+
 
     <!-------------------    title of the table ------------------------------------------------->
-    <div class='list-group-item bold-italic-custom'>
+    <div class='list-group-item' style= "font-weight:bold; font-style: italic; background-color:#e3dfa5">
         <div class="row show-grid">
             <div class="col-1">
-                <!-- <?php __p( 'image' ); ?> -->
+                Photo &nbsp;
             </div>
             <div class="col-4">
-                <?php __p( 'fanpage_name' ); ?> &nbsp;
-                <a href="#" class="icons fans-pages-names-desc"><i class="icon-arrow-down-white"></i></a>
-                <a href="#" class="icons fans-pages-names-asc"><i class="icon-arrow-up-white"></i></a>
+                Fan page name &nbsp;
+                <a href="#" class="icons fans-pages-names-desc">&nbsp&nbsp&nbsp<i class="icon-long-arrow-down"></i>&nbsp&nbsp&nbsp</a>
+                <a href="#" class="icons fans-pages-names-asc">&nbsp&nbsp&nbsp<i class="icon-long-arrow-up"></i>&nbsp&nbsp&nbsp</a>
             </div>
             <div class="col-3">
-                <?php __p( 'likes' ); ?> &nbsp;
-                <a href="#" class="icons likes-desc"><i class="icon-arrow-down-white"></i></a>
-                <a href="#" class="icons likes-asc"><i class="icon-arrow-up-white"></i></a>
+                Likes &nbsp;
+                <a href="#" class="icons likes-desc">&nbsp&nbsp&nbsp<i class="icon-long-arrow-down"></i>&nbsp&nbsp&nbsp</a>
+                <a href="#" class="icons likes-asc">&nbsp&nbsp&nbsp<i class="icon-long-arrow-up"></i>&nbsp&nbsp&nbsp</a>
             </div>
             <div class="col-3">
-                <?php __p( 'talking_about' ); ?> &nbsp;
-                <a href="#" class="icons talks-about-desc"><i class="icon-arrow-down-white"></i></a>
-                <a href="#" class="icons talks-about-asc"><i class="icon-arrow-up-white"></i></a>
+                Talking about &nbsp;
+                <a href="#" class="icons talks-about-desc">&nbsp&nbsp&nbsp<i class="icon-long-arrow-down"></i>&nbsp&nbsp&nbsp</a>
+                <a href="#" class="icons talks-about-asc">&nbsp&nbsp&nbsp<i class="icon-long-arrow-up"></i>&nbsp&nbsp&nbsp</a>
             </div>
             <div class="col-1">
-                <?php __p( 'expand' ); ?>
+                Expand
             </div>
         </div>
     </div>
-    <div class='page-spinner'><i class='icon-spinner icon-spin icon-large'></i></div>
+    <div class='page-spinner'><i class='icon-spinner icon-spin icon-large' style='font-size:80px;'></i></div>
 
 
 
@@ -183,23 +225,6 @@ if (isset($aaForJs['instance']['fb_app_secret']))
 
 </div>
 
-<?php
-/*
-<div id="custom_footer">
-    < ?php echo (isset( $aa[ 'config' ][ 'custom_footer' ][ 'value' ] ) ) ? $aa[ 'config' ][ 'custom_footer' ][ 'value' ] : ''; ? >
-</div>
-*/
-?>
-<div id="custom_footer">
- 	<div class="main_container">
-	    <div class="left">made with love in Cologne © <?php echo date('Y') ?> <a href="http://app-arena.com/" target="_blank" title="App-Arena.com">App-Arena.com</a></div>
-	    <div class="right"><a href="#" title="Impressum">Impressum</a> | <a href="#" title="AGB">AGB</a></div>
- 	</div>
-</div>
-
-<div id="form-wrapper" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
-
-</div>
 
 <?php
 //unset($aa['config']['css_bootstrap']);
@@ -214,21 +239,7 @@ if (isset($aaForJs['instance']['fb_app_secret']))
 <script>
     var aa = <?php echo json_encode($aaForJs); ?>; // copy aa as a global object to js
 </script>
-<script src="js/vendor/jquery/jquery.js"></script>
-<script src="js/vendor/bootstrap-assets/js/select.min.js"></script>
 <script src="js/config.js"></script>
 <script id="requirejs" data-main="js/main" data-devmode="1" src="js/vendor/requirejs/require.js"></script>
-<script>
-
-    jQuery( 'body' ).ready(
-    	function()
-    	{
-
-	    	jQuery('select#max-rows').selectpicker();
-
-    	}
-    );
-
-</script>
 </body>
 </html>
