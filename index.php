8fdf89de0fe6dd6504b4ce0957eb518a3d5ada51
maerz-illustratics <maerz@illustratics.com>
2013-10-09 11:43:26 +0200

    Test

--- index.php
+++ index.php
@@ -59,8 +59,7 @@ if (isset($aaForJs['instance']['fb_app_secret']))
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>..::: Volksbanks fan page ranking App :::..</title>
+    <title>..: Volksbanks fan page ranking App :..</title>
 
     <link type="text/css" rel="stylesheet" href="css/style.css" />
     <link type="text/css" rel="stylesheet" href="css/ranking.css" />
